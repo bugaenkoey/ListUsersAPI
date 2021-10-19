c558dfbdaa030d4d1e19917cdb11374a819dd22c
Yevhen <buhaienkoyevhen@gmail.com>
2021-10-20 02:01:47 +0300

    in Startup.cs services.AddCors(options => options.AddPolicy("devCors", opts => opts .AllowAnyOrigin() .AllowAnyHeader() .AllowAnyMethod()));
    app.UseCors("devCors");

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -5,65 +5,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Text.Json;
+using System.IO;
 
 namespace ListUsersAPI.Controllers
 {
     public class User
     {
-        /* id: number;
-  name: string;
-  username: string;
-  email?: string;
-          */
+
         public int id { get; set; }
         public string name { get; set; }
         public string username { get; set; }
         public string email { get; set; }
 
-        
+
     }
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-       static  User[] users = new User[] { 
-           new User { id = 111, name = "Jekky", username = "Jekson", email = "JJ@gmaik.com" },
-        new User { id = 112, name = "Bob", username = "Bobik", email = "BB@gmaik.com" },
-         new User { id = 113, name = "Janet", username = "Jekson", email = "JanetJekson@gmaik.com" }
-       };
-        // new User( 112, "Bob", "Bobik", "BB@gmaik.com"),
-        // new User( 113, "Janet", "Jekson", "JanetJekson@gmaik.com"),
-        //     };
-        string json = JsonSerializer.Serialize<User[]>(users);
-
-        public string Json { get => json; set => json = value; }
-
-    
-
-        // string listUserStr = "[{'id': 1,'username': 'Vasa','name':'Vasiliev','email': 'Vasa@email.com'  }, {    'id': 2,    'username': 'Kolya',    'name': 'Nikolaev',    'email': 'Kolya@email.com'  },  {    'id': 3,    'username': 'Petya',    'name': 'Petrov',    'email': 'Petya@email.com'  },  {    'id': 4,       'username': 'Zina',       'name': 'Zinger',       'email': 'Zina@email.com'     }]";
+       
         [HttpGet]
-        public string Get()
+        public async Task<User[]> Get()
         {
+            using FileStream openStream = System.IO.File.OpenRead(@"C:\Users\asus\source\repos\ListUsersAPI\ListUserJson\users.json");
+            User[] users2 = await JsonSerializer.DeserializeAsync<User[]>(openStream);
 
-             string jsonString = System.IO.File.ReadAllText(@"C:\Users\asus\source\repos\ListUsersAPI\ListUserJson\users.json");
- /*           User[] user5 = JsonSerializer.Deserialize<User[]>(jsonString);
-            Console.WriteLine(user5);
-            Console.WriteLine(json);
- */
-            return jsonString;
+            return users2;
         }
-     /*   public IEnumerable<string> Get()
-        {
-            string[] list = new string[] { "Value 1", "Value 2", "Value 3", "Value 4", "Value 5" };
-            return list;
-        }
-     */
+      
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "this id - " + id;
         }
 
+        [HttpPost]
+        public void Post()
+        {
+            
+        }
+
+        [HttpPut]
+        public string Put()
+        {
+            return "**PUT**";
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteUser(int id)
+        {
+            
+        }
 
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -32,6 +32,13 @@ namespace ListUsersAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ListUsersAPI", Version = "v1" });
             });
+            //++++++++++++++++++++++++++++++++
+            services.AddCors(
+               options => options.AddPolicy("devCors", opts => opts
+               .AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod()));
+            //---------------------------------
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +49,11 @@ namespace ListUsersAPI
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ListUsersAPI v1"));
+
+                //+++++++++++++++++++++++++++++++++
+                app.UseCors("devCors");
+                //-----------------------------------
+
             }
 
             app.UseHttpsRedirection();
